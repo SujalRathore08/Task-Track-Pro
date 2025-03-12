991d243eeafba2ba70b40cccb4a3c51659c46b94
DhruvilBarvaliya <dhruvilbarvaliya20@gmail.com>
2025-03-12 16:20:22 +0530

    Folder structure setup

---
+++ src/TaskTrackPro.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TaskTrackPro.MVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
